10398485bd1c45050e88ac1a100cb58b3aa72522
Eric Scouten <eric@scouten.com>
2022-04-12 22:04:36 -0700

    Found a way through that. The ergonomics here are ... not wonderful.

--- demo.sh
+++ demo.sh
@@ -4,7 +4,11 @@ rustup component add llvm-tools-preview
 
 cargo install rustfilt
 
-RUSTFLAGS="-C instrument-coverage" cargo test
+brew install jq
+
+RUSTFLAGS="-C instrument-coverage" cargo test --no-run --message-format=json | jq -r "select(.profile.test == true) | .filenames[]" > .test-bin-name
+
+`cat .test-bin-name`
 
 $(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-profdata merge -sparse default.profraw -o default.profdata
 
@@ -14,7 +18,9 @@ $(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-profdata merg
 # What should we do instead to test coverage in a library
 # when executed via cargo test?
 
-$(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-cov show -Xdemangler=rustfilt target/debug/deps/librust_coverage_sandbox.rlib \
+$(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-cov show  \
+    -Xdemangler=rustfilt \
+    `cat .test-bin-name` \
     -instr-profile=default.profdata \
     -show-line-counts-or-regions \
     -show-instantiations
