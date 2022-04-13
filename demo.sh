065476e41b268ea23e39d661168d5e5ae650909c
Eric Scouten <eric@scouten.com>
2022-04-12 21:09:27 -0700

    Adapt example to use a library.
    Can we test library coverage via `cargo test`? No obvious path to that yet.

--- demo.sh
+++ demo.sh
@@ -4,11 +4,17 @@ rustup component add llvm-tools-preview
 
 cargo install rustfilt
 
-RUSTFLAGS="-C instrument-coverage" cargo run
+RUSTFLAGS="-C instrument-coverage" cargo test
 
 $(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-profdata merge -sparse default.profraw -o default.profdata
 
-$(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-cov show -Xdemangler=rustfilt target/debug/rust-coverage-sandbox \
+# DEMO FAILS HERE:
+# error: target/debug/deps/librust_coverage_sandbox.rlib: Failed to load coverage: No coverage data found
+
+# What should we do instead to test coverage in a library
+# when executed via cargo test?
+
+$(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-cov show -Xdemangler=rustfilt target/debug/deps/librust_coverage_sandbox.rlib \
     -instr-profile=default.profdata \
     -show-line-counts-or-regions \
     -show-instantiations
