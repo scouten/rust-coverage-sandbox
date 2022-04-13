823984bde2e0e972560eeef2dd336e1fb2837255
Eric Scouten <eric@scouten.com>
2022-04-12 22:19:51 -0700

    Extend demo to show some uncovered lines.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
-pub fn hello() {
-    println!("Hello, world!");
+pub fn hello(answer: u16) {
+    if answer == 42 {
+        println!("Hello, world!");
+    } else {
+        println!("I have no idea what you're talking about.");
+    }
 }
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn test_hello() {
-        super::hello()
+        super::hello(42)
     }
 }
