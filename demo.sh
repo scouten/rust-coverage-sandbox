f7a17af75617ec495ef37c95c3d7480f05849648
Eric Scouten <eric@scouten.com>
2022-04-12 22:06:59 -0700

    Remove comments that are no longer relevant.

--- demo.sh
+++ demo.sh
@@ -12,12 +12,6 @@ RUSTFLAGS="-C instrument-coverage" cargo test --no-run --message-format=json | j
 
 $(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-profdata merge -sparse default.profraw -o default.profdata
 
-# DEMO FAILS HERE:
-# error: target/debug/deps/librust_coverage_sandbox.rlib: Failed to load coverage: No coverage data found
-
-# What should we do instead to test coverage in a library
-# when executed via cargo test?
-
 $(rustc --print sysroot)/lib/rustlib/aarch64-apple-darwin/bin/llvm-cov show  \
     -Xdemangler=rustfilt \
     `cat .test-bin-name` \
